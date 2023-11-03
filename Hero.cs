dc1c2e92ed266e1fec44c23cc35bf54663e9ce8e
Brenda Brito <bbritob8@gmail.com>
2023-11-03 15:33:49 -0300

    Incluindo classe hero

---
+++ Hero.cs
@@ -0,0 +1,39 @@
+﻿public class Hero
+{
+    public string Name {get; set;}
+    public int Age {get; set;}
+    public string Type {get; set;}
+
+    public Hero (string name, int age, string type)
+    {
+        Name = name;
+        Age = age;
+        Type = type;
+    }
+
+    public void Attack()
+    {
+        string attack = "";
+
+        switch (Type)
+        {
+            case "mage":
+                attack = "uses magic";
+                break;
+            case "warrior":
+                attack = "uses sword";
+                break;
+            case "monk":
+                attack = "uses martial arts";
+                break;
+            case "ninja":
+                attack = "uses shuriken";
+                break;
+            default:
+                attack = "carried out an attack";
+                break;
+        }
+
+        Console.WriteLine($"The {Type} attacked using  {attack}");
+    }
+}
