daf5c0191008ab8072927dfef7ac96906c506539
Brenda Brito <bbritob8@gmail.com>
2023-11-03 15:33:59 -0300

    incluindo chamado da classe

---
+++ Program.cs
@@ -0,0 +1,11 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criar uma instância da classe Heroi
+        Hero hero = new Hero("Bibi", 19, "ninja");
+
+        // Chamar o método Atacar
+        hero.Attack(); 
+    }
+}
